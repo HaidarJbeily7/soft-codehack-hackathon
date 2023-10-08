a631d26d29150189a449b336966ac42abbd3e894
Hamada Salhab <hamada.a.salhab@gmail.com>
2023-10-08 12:43:02 +0300

    Add inferred tags when adding post

--- ai-services/addPost.py
+++ ai-services/addPost.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from flask import jsonify
+from inferTags import infer_tags
+import json
 
 csv_file_path = './data/posts.csv'
 
@@ -10,17 +12,27 @@ def add_post(data):
     post_description = data.get('post_description', '')
     post_tags = data.get('post_tags', '')
     post_publishing_date = data.get('post_publishing_date', '')
-
+    inferred_tags = infer_tags(data)
     new_post = {
         'post_id': post_id,
         'title': title,
         'post_description': post_description,
         'post_tags': post_tags,
         'post_publishing_date': post_publishing_date,
+        'inferred_tags': inferred_tags.get('inferred_tags')
     }
 
-    # Append the new row to the CSV file
-    with open(csv_file_path, 'a') as f:
+    try: 
+        f = open(csv_file_path, 'a')
         pd.DataFrame([new_post]).to_csv(f, header=False, index=False)
-
-    return jsonify({'message': 'New post added successfully'})
+    except Exception as e:
+        print(e)
+        fail = {
+            "message": e
+        }
+        return fail
+    finally:
+        success = {
+            "message": "New post added successfully"
+        }
+        return success
