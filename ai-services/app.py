82f19c460e7fc710652c245a13db416dfce4fda5
Hamada Salhab <hamada.a.salhab@gmail.com>
2023-10-08 12:44:54 +0300

    Add add_post route & Refactor

--- ai-services/app.py
+++ ai-services/app.py
@@ -1,35 +1,32 @@
-from flask import Flask, request
-import json
-from inferTags import infer_tags
-from addPostInteraction import add_post_interaction
+from flask import Flask, request, jsonify
 from addPost import add_post
+from addFreelancer import add_freelancer
+from addPostInteraction import add_post_interaction
+from inferTags import infer_tags
 from recommendPosts import recommend_posts
 import nltk
 
 app = Flask(__name__)
 
-@app.route('/infer_tags', methods=['POST'])
-def infer_tags_route():
-    data = request.get_json()
-    return infer_tags(data, max_tags=5)
-
 @app.route('/add_post', methods=['POST'])
 def add_post_route():
-    data = request.get_json()
-    return add_post(data)
+    return jsonify(add_post(data=request.get_json()))
+
+@app.route('/add_freelancer', methods=['POST'])
+def add_freelancer_route():
+    return jsonify(add_freelancer(data=request.get_json()))
 
 @app.route('/add_post_interaction', methods=['POST'])
 def add_post_interaction_route():
-    data = request.get_json()
-    return add_post_interaction(data)
+    return jsonify(add_post_interaction(data=request.get_json()))
+
+@app.route('/infer_tags', methods=['GET'])
+def infer_tags_route():
+    return jsonify(infer_tags(data=request.get_json()))
 
 @app.route('/recommend_posts', methods=['GET'])
 def recommend_posts_route():
-    data = request.get_json()
-    freelancer_address = data['freelancer_address']
-    # print(freelancer_address)
-    # return "BLYAT"
-    return json.dumps(recommend_posts(freelancer_address=freelancer_address))
+    return jsonify(recommend_posts(data=request.get_json()))
 
 if __name__ == '__main__':
     nltk.download('stopwords', quiet=True)
